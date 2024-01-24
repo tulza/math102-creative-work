3167768c2f760512abb20b188ff232bcc97f0f19
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-25 04:17:09 +1300

    working page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 import HomePage from "@pages/HomePage";
 import "@styles/styles.css";
 import "@styles/fonts.css";
-import LoadingPage from "./components/pages/LoadingPage";
+import LoadingPage from "@/components/pages/LoadingPage";
+import TransitionAnim from "@/components/TransitionAnim";
+import { createContext, useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+export const TransitionContext = createContext<any>(null);
 
 function App() {
+  const [transition, setTransition] = useState<string | null>(null);
+  const handleTransitionTo = (link: string) => {
+    setTransition(link);
+  };
+  const handleFinishTransition = () => {
+    setTransition(null);
+  };
+  useEffect(() => {
+    console.log(transition);
+  }, [transition]);
+
   return (
     <>
-      <LoadingPage />
-      <TransitionAnim />
-      <HomePage />
+      <TransitionContext.Provider
+        value={{
+          handleTransitionTo: handleTransitionTo,
+          handleFinishTransition: handleFinishTransition,
+        }}
+      >
+        <Router basename="math102-creative-work">
+          {!!transition && <TransitionAnim />}
+          <Routes>
+            <Route path="/start" element={<LoadingPage />} />
+            <Route path="/main" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/start" />} />
+          </Routes>
+        </Router>
+      </TransitionContext.Provider>
     </>
   );
 }
---
+++ src/components/TransitionAnim.tsx
@@ -0,0 +1,21 @@
+import { TransitionContext } from "@/App";
+import { motion } from "framer-motion";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+const TransitionAnim = () => {
+  const navigate = useNavigate();
+  const { handleFinishTransition } = useContext(TransitionContext);
+  const transitionDuration = 2;
+  setTimeout(() => navigate("main"), (transitionDuration * 1000) / 2);
+  setTimeout(handleFinishTransition, transitionDuration * 1000);
+  return (
+    <motion.div
+      className="absolute z-50 h-[100vh] w-[100vw] bg-black"
+      animate={{ x: ["120%", "0%", "0%", "120%"] }}
+      transition={{ duration: transitionDuration }}
+    />
+  );
+};
+
+export default TransitionAnim;
