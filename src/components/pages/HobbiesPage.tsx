3da509fba18452694653498b8aa3d4044158c8a5
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-29 08:44:58 +1300

    hobbies page initial

--- src/components/pages/HobbiesPage.tsx
+++ src/components/pages/HobbiesPage.tsx
@@ -4,6 +4,9 @@ import { useContext } from "react";
 import { ThemeContext, TransitionContext } from "@/App";
 import useMousePos from "@/hooks/useMousePos";
 import { useScreenSize } from "@/hooks/useScreenSize";
+import { motion } from "framer-motion";
+import InvertBypass from "../invertBypass";
+import { CursorTrackCode, ParallaxCode } from "@/images/imageExport";
 
 const HobbiesPage = () => {
   const { mouseX, mouseY } = useMousePos();
@@ -18,17 +21,17 @@ const HobbiesPage = () => {
 
     return c;
   };
+  const calcAngleDegrees = () => {
+    return (Math.atan2(getY(), getX()) * 180) / Math.PI;
+  };
   const getX = () => {
     return mouseX - screenX / 2;
   };
   const getY = () => {
     return mouseY - screenY / 2;
   };
-  const calcAngleDegrees = () => {
-    return (Math.atan2(getY(), getX()) * 180) / Math.PI;
-  };
   return (
-    <div className="bold relative flex h-full w-full select-none bg-white p-10">
+    <div className="bold relative flex h-full w-full select-none justify-center bg-white p-10">
       <Button
         text={isDarkmode ? "Light Mode" : "Dark Mode"}
         className="absolute right-10 top-10 z-30"
@@ -44,15 +47,105 @@ const HobbiesPage = () => {
         }}
       />
 
-      <div
-        className="absolute left-[50%] top-[50%] z-10  h-2 w-2 whitespace-nowrap  rounded-full bg-black"
-        style={{ rotate: `${calcAngleDegrees()}deg` }}
-      >
-        <div
-          className="my-1 h-[1px] rounded-full bg-black"
-          style={{ width: handleWidth() }}
-        ></div>
+      <CursorTrack
+        calcAngleDegrees={calcAngleDegrees}
+        handleWidth={handleWidth}
+      />
+
+      <div className="relative flex w-[1200px] items-center">
+        <motion.div
+          whileHover="hover"
+          className="absolute left-0  flex flex-col"
+          style={parallax(mouseX, mouseY, 0.01)}
+        >
+          <div className="z-10 w-max cursor-pointer rounded-lg bg-white p-2 px-6 text-3xl outline">
+            Web development
+          </div>
+          <motion.div className="w-[600px] overflow-hidden pt-4">
+            <motion.div
+              className="h-0 rounded-lg border-0 border-black"
+              variants={{ hover: { height: 600, borderWidth: 0 } }}
+            >
+              <div className="light h-full w-full p-4">
+                <p className="mb-2">
+                  T: <InvertBypass>👋</InvertBypass> Many of the components I've
+                  made on this website likely have had some math included in
+                  some form, from the parallax effect
+                </p>
+                <InvertBypass>
+                  <div className="flex w-full">
+                    <img src={ParallaxCode} />
+                  </div>
+                </InvertBypass>
+                <p className="flex justify-end">
+                  to the cursor track over here --&gt;
+                </p>
+                <InvertBypass>
+                  <div className="flex w-full px-[50px]">
+                    <img src={CursorTrackCode} />
+                  </div>
+                </InvertBypass>
+                <p>
+                  so it can be said that{" "}
+                  <span className="bold mb-2">
+                    Math has a pretty significant impacts here,
+                  </span>
+                </p>
+                <p>
+                  and am thankful that it had allowing me to create the
+                  aesthetics of the web that i have envisioned.
+                </p>
+              </div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+        {/*  */}
+        <motion.div
+          whileHover="hover"
+          className="absolute right-0 flex flex-col items-end"
+          style={parallax(mouseX, mouseY, 0.01)}
+        >
+          <div className="z-10 w-max cursor-pointer rounded-lg bg-white p-2 px-6 text-3xl outline">
+            Cooking
+          </div>
+          <motion.div className="w-[400px] overflow-hidden pt-4">
+            <motion.div
+              className="h-0 rounded-lg border-0 border-black"
+              variants={{ hover: { height: 200, borderWidth: 0 } }}
+            >
+              <div className="light h-full w-full p-4">
+                <ol style={{ listStyleType: "-moz-initial" }}>
+                  <li>I want to cook yummy food and drinks</li>
+                  <li>I follow a recipe</li>
+                  <li>follow the measurement</li>
+                  <li>follow steps and cooking time</li>
+                  <li>food comes out great thanks to numbers</li>
+                </ol>
+              </div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
       </div>
+    </div>
+  );
+};
+
+const CursorTrack = ({
+  calcAngleDegrees,
+  handleWidth,
+}: {
+  calcAngleDegrees: any;
+  handleWidth: any;
+}) => {
+  return (
+    <div
+      className="absolute left-[50%] top-[50%] z-10  h-2 w-2 whitespace-nowrap  rounded-full bg-black"
+      style={{ rotate: `${calcAngleDegrees()}deg` }}
+    >
+      <div
+        className="my-1 h-[1px] rounded-full bg-black"
+        style={{ width: handleWidth() }}
+      ></div>
     </div>
   );
 };
--- src/images/imageExport.js
+++ src/images/imageExport.js
@@ -2,5 +2,14 @@ import LeftArrow from "./left-arrow.svg";
 import RightArrow from "./right-arrow.svg";
 import IconLight from "./IconLight.svg";
 import IconDark from "./IconDark.svg";
+import CursorTrackCode from "./cursorTrack.png";
+import ParallaxCode from "./parallax.png";
 
-export { LeftArrow, RightArrow, IconLight, IconDark };
+export {
+  LeftArrow,
+  RightArrow,
+  IconLight,
+  IconDark,
+  CursorTrackCode,
+  ParallaxCode,
+};
