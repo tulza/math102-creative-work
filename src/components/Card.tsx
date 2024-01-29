31ccb32d65159c91cd70686d6d9237f4bdc59cbf
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-30 03:19:05 +1300

    education page content added

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type CardProps = {
+  children?: React.ReactNode;
+  text: string;
+  description: string;
+};
+const Card = ({ children, text, description }: CardProps) => {
+  return (
+    <div className="min-h-[200px] w-[400px] rounded-lg border-4 border-black p-6">
+      <p className="bold mb-4 text-xl">{text}</p>
+      <p>{description}</p>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
--- src/components/pages/EducationPage.tsx
+++ src/components/pages/EducationPage.tsx
@@ -1,8 +1,10 @@
 import { parallax } from "@/styles/parallaxStyle";
-import Button from "../Button";
 import { useContext } from "react";
 import { ThemeContext, TransitionContext } from "@/App";
 import useMousePos from "@/hooks/useMousePos";
+import Button from "@components/Button";
+import Card from "@components/Card";
+import InvertBypass from "@components/InvertBypass";
 
 const EducationPage = () => {
   const { mouseX, mouseY } = useMousePos();
@@ -24,7 +26,52 @@ const EducationPage = () => {
           handleTransitionTo("main");
         }}
       />
-      <div className="mt-20">Yo</div>
+      <div
+        className="light mt-20 flex w-full flex-col items-center px-10"
+        style={parallax(mouseX, mouseY, 0.02)}
+      >
+        <p className="bold text-3xl">My major is computer science.</p>
+        <p className="bold mb-4 text-xl">
+          so, how does it apply to my courses and studies?
+        </p>
+        <p>Overall computer science is a fairly math heavy field</p>
+        <p>so, am I happy about it?</p>
+        <p className="bold my-4">
+          well here are some of my takes of math previous classes.{" "}
+          <InvertBypass>🔥</InvertBypass>
+        </p>
+        <hr className=" mb-10 w-full border-DarkGray" />
+        <div
+          className="mb-4 flex justify-center gap-4"
+          style={parallax(mouseX, mouseY, 0.02)}
+        >
+          <Card
+            text="Compsci130"
+            description="while the math were very basic, all logics use math"
+          />
+          <Card
+            text="Physics140"
+            description="uses of boolean in this course were definitely interesting as simple as it is, the many way it could be expressed and use were very interesting"
+          />
+          <Card
+            text="Stats101"
+            description="Math is very much expressed in percentage in this course, and the use of it to forecast events, I found that pretty cool"
+          />
+        </div>
+        <div
+          className="flex justify-center gap-4"
+          style={parallax(mouseX, mouseY, 0.01)}
+        >
+          <Card
+            text="Compsci110"
+            description="boolean algebra used were far more advance and have cause many headache i rate math 10/10 "
+          />
+          <Card
+            text="so what do i think?"
+            description="In at least compsci, math is everywhere, and I already use It. It really subjective and depends on how creative I use or apply it to anything"
+          />
+        </div>
+      </div>
     </div>
   );
 };
